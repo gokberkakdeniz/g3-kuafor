86306def6d362a47edd2a7baa324b733d9e54f72
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-05-17 22:06:20 +0300

    feat: homepage partially implemented

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Main = ({ children }) => {
-  return <main className="p-4">{children}</main>;
+  return (
+    <main className="p-4" style={{ height: "calc(100% - 75px)" }}>
+      {children}
+    </main>
+  );
 };
 
 export default Main;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,32 @@ import { Button } from "../../components";
 
 const Home = () => {
   return (
-    <div>
-      <Link to="/employees">
-        <Button type="button" onClick={console.log}>
-          Employees
-        </Button>
-      </Link>
+    <div className="flex justify-center items-center h-full">
+      <div className="m-2">
+        <Link to="/employees">
+          <div className="text-center">
+            <img
+              src="https://via.placeholder.com/512x512"
+              alt=""
+              className="md:w-96 md:h-96 sm:w-48 sm:h-48 h-24 w-24 mx-auto"
+            />
+            <h2 className="font-bold font-serif md:text-xl sm:text-lg text-md p-4">Employees</h2>
+          </div>
+        </Link>
+      </div>
+
+      <div className="m-2">
+        <Link to="/calendar">
+          <div className="text-center">
+            <img
+              src="https://via.placeholder.com/512x512"
+              alt=""
+              className="md:w-96 md:h-96 sm:w-48 sm:h-48 h-24 w-24 mx-auto"
+            />
+            <h2 className="font-bold font-serif md:text-xl sm:text-lg text-md p-4">Calendar</h2>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
