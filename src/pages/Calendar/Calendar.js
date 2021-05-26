42d10b6a0c9f448072ef97af73be1963f76bd6fd
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-05-26 15:12:28 +0300

    feat: calendar lib added

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -2,9 +2,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IoArrowForward, IoArrowBack } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import Dayzed, { useDayzed } from "dayzed";
 import { Button, BackButton } from "../../components/Button";
 import { decrease, increase } from "../../store/counter";
-import { NameDiv, Titles } from "../../components";
+import { NameDiv } from "../../components";
 import Users from "../../store/employees";
 
 const Calendar = () => {
@@ -12,6 +13,101 @@ const Calendar = () => {
   const dispatch = useDispatch();
   const onIncrement = () => dispatch(increase());
   const onDecrement = () => dispatch(decrease());
+  const monthNamesShort = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  const weekdayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  function Malendar({ calendars, getBackProps, getForwardProps, getDateProps }) {
+    if (calendars.length) {
+      return (
+        <div style={{ maxWidth: 800, margin: "0 auto", textAlign: "center" }}>
+          <div>
+            <button type="button" {...getBackProps({ calendars })}>
+              Back
+            </button>
+            <button type="button" {...getForwardProps({ calendars })}>
+              Next
+            </button>
+          </div>
+          {calendars.map((calendar) => (
+            <div
+              key={`${calendar.month}${calendar.year}`}
+              style={{
+                display: "inline-block",
+                width: "50%",
+                padding: "0 10px 30px",
+                boxSizing: "border-box"
+              }}>
+              <div>
+                {monthNamesShort[calendar.month]} {calendar.year}
+              </div>
+              {weekdayNamesShort.map((weekday) => (
+                <div
+                  key={`${calendar.month}${calendar.year}${weekday}`}
+                  style={{
+                    display: "inline-block",
+                    width: "calc(100% / 7)",
+                    border: "none",
+                    background: "transparent"
+                  }}>
+                  {weekday}
+                </div>
+              ))}
+              {calendar.weeks.map((week, weekIndex) =>
+                week.map((dateObj, index) => {
+                  const key = `${calendar.month}${calendar.year}${weekIndex}${index}`;
+                  if (!dateObj) {
+                    return (
+                      <div
+                        key={key}
+                        style={{
+                          display: "inline-block",
+                          width: "calc(100% / 7)",
+                          border: "none",
+                          background: "transparent"
+                        }}
+                      />
+                    );
+                  }
+                  const { date, selected, selectable, today } = dateObj;
+                  let background = today ? "cornflowerblue" : "";
+                  background = selected ? "purple" : background;
+                  background = !selectable ? "teal" : background;
+                  return (
+                    <button
+                      type="button"
+                      style={{
+                        display: "inline-block",
+                        width: "calc(100% / 7)",
+                        border: "none",
+                        background: "black"
+                      }}
+                      key={key}
+                      {...getDateProps({ dateObj })}>
+                      {selectable ? date.getDate() : "X"}
+                    </button>
+                  );
+                })
+              )}
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  }
 
   return (
     <>
@@ -20,6 +116,7 @@ const Calendar = () => {
           <IoArrowBack color="#e6e6e6" size="2em" />
         </BackButton>
       </Link>
+
       <div className="flex flex-col space-y-2 justify-center items-center">CALENDAR</div>
     </>
   );
