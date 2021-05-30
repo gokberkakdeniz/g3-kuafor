c3fef868d344e1fb3e8c0f8c002c4a851882d2b5
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-05-30 10:52:27 +0300

    fix: make Title component responsive

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,9 @@ import React from "react";
 
 const Title = ({ style = {}, className = "", title }) => {
   return (
-    <div className={clsx(className, "flex w-80 h-12 rounded-3xl px-3 bg-header")} style={style}>
+    <div
+      className={clsx(className, "flex lg:w-80 lg:h-12 rounded-3xl px-3 bg-header")}
+      style={style}>
       <h2 className="text-2xl font-Open-Sans font-semibold m-auto text-black">{title}</h2>
     </div>
   );
